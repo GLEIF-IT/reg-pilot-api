c285742db0289677112a71f4aa6fa1e2bb5ec3e8
Aidar Negimatzhanov <aydar.negimatzhanov@perfectart.com>
2025-01-27 22:58:40 +0100

    Added vlei-verifier-client support

--- src/regps/app/api/controllers.py
+++ src/regps/app/api/controllers.py
@@ -1,71 +1,73 @@
+import os
+
 import requests
-from regps.app.adapters.verifier_service_adapter import VerifierServiceAdapter, FilerServiceAdapter
+from regps.app.adapters.filer_service_adapter import FilerServiceAdapter
 from regps.app.api.exceptions import (
     VerifierServiceException,
     DigestVerificationFailedException,
 )
 from regps.app.api.digest_verifier import verify_digest
+from vlei_verifier_client import VerifierClient, VerifierResponse
 
 
 class APIController:
     def __init__(self):
-        self.verifier_adapter = VerifierServiceAdapter()
+        verifier_base_url = os.environ.get("VLEI_VERIFIER", "http://localhost:7676")
+        self.verifier_client = VerifierClient(verifier_base_url)
         self.filer_adapter = FilerServiceAdapter()
 
     def check_login(self, aid: str):
-        verifier_response: requests.Response = (
-            self.verifier_adapter.check_login_request(aid)
-        )
-        if verifier_response.status_code != 200:
+        verifier_response: VerifierResponse = self.verifier_client.check_login(aid)
+        if verifier_response.code != 200:
             raise VerifierServiceException(
-                verifier_response.json(), verifier_response.status_code
+                verifier_response.message, verifier_response.code
             )
-        return verifier_response.json()
+        return verifier_response.body
 
     def login(self, said: str, vlei: str):
-        verifier_response = self.verifier_adapter.verify_vlei_request(said, vlei)
-        if verifier_response.status_code != 202:
+        verifier_response: VerifierResponse = self.verifier_client.login(said, vlei)
+        if verifier_response.code != 202:
             raise VerifierServiceException(
-                verifier_response.json(), verifier_response.status_code
+                verifier_response.message, verifier_response.code
             )
-        return verifier_response.json()
+        return verifier_response.body
 
     def add_root_of_trust(self, aid, vlei, oobi):
-        verifier_response = self.verifier_adapter.add_root_of_trust_request(aid, vlei, oobi)
-        if verifier_response.status_code != 202:
+        verifier_response: VerifierResponse = self.verifier_client.add_root_of_trust(aid, vlei, oobi)
+        if verifier_response.code != 202:
             raise VerifierServiceException(
-                verifier_response.json(), verifier_response.status_code
+                verifier_response.message, verifier_response.code
             )
-        return verifier_response.json()
+        return verifier_response.body
 
     def verify_cig(self, aid, cig, ser):
-        verifier_response = self.verifier_adapter.verify_cig_request(aid, cig, ser)
-        if verifier_response.status_code != 202:
+        verifier_response: VerifierResponse = self.verifier_client.verify_signed_headers(aid, cig, ser)
+        if verifier_response.code != 202:
             raise VerifierServiceException(
-                verifier_response.json(), verifier_response.status_code
+                verifier_response.message, verifier_response.code
             )
-        return verifier_response.json()
+        return verifier_response.body
 
     def check_upload(self, aid: str, dig: str):
-        verifier_response = self.filer_adapter.check_upload_request(aid, dig)
-        if verifier_response.status_code != 200:
+        filer_response = self.filer_adapter.check_upload_request(aid, dig)
+        if filer_response.status_code != 200:
             raise VerifierServiceException(
-                verifier_response.json(), verifier_response.status_code
+                filer_response.json(), filer_response.status_code
             )
-        return verifier_response.json()
+        return filer_response.json()
 
     def get_upload_statuses_admin(self, aid: str, lei: str):
-        verifier_response = self.filer_adapter.upload_statuses_admin_request(aid, lei)
-        if verifier_response.status_code != 200:
+        filer_response = self.filer_adapter.upload_statuses_admin_request(aid, lei)
+        if filer_response.status_code != 200:
             raise VerifierServiceException(
-                verifier_response.json(), verifier_response.status_code
+                filer_response.json(), filer_response.status_code
             )
-        return verifier_response.json()
+        return filer_response.json()
 
     def upload(self, aid: str, dig: str, report: bytes, contype: str, raw):
         if not verify_digest(report, dig):
             raise DigestVerificationFailedException(
                 "Report digest verification failed", 400
             )
-        verifier_response = self.filer_adapter.upload_request(aid, dig, contype, raw)
-        return verifier_response
+        filer_response = self.filer_adapter.upload_request(aid, dig, contype, raw)
+        return filer_response
